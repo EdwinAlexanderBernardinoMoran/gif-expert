d231adc129db3bdcae8b069cd1979468955b25bd
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-01 09:31:46 -0600

    S6- GifGrid - Nuevo Componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
@@ -24,13 +25,11 @@ export const GifExpertApp = () => {
       <AddCategory onNewCategory={ onAddCategory }/>
 
       {/* Listado de Gif */}
-      <ol>
-        {
-          categories.map(category => (
-            <li key={category}>{category}</li>
-          ))
-        }
-      </ol>
+      {
+        categories.map((category) => (
+          <GifGrid key={category} category={ category }/>
+        ))
+      }
     </>
   )
 }
