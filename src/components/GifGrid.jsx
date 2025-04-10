c627494fcac9e19d80a674bb5b392bf5a2364078
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-09 19:53:06 -0600

    S6- Mostrar los titulos de las imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,19 +3,33 @@ import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
     
-    const [counter, setcounter] = useState(10)
+    // const [counter, setcounter] = useState(10)
+    const [images, setImages] = useState([])
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
 
     useEffect(() => {
-      getGifs(category);
-
+      getImages();
     }, [])
     
   return (
     <>
-        <h3>{category}</h3>
-        <h5>{ counter }</h5>
-        <button onClick={ () => setcounter(counter + 1)}>+1</button>
-        <p>Lorem ipsum dolor sit.</p>
+      <h3>{category}</h3>
+      {/* <h5>{ counter }</h5>
+      <button onClick={ () => setcounter(counter + 1)}>+1</button> */}
+      <ol>
+        {
+          images.map( ({ id, title, url }) => (
+            <li key={id}>
+            <p>{title}</p>
+            <img src={url} alt={title} />
+            </li>
+          ))
+        }
+      </ol>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=y6lJHsvEGIkn7javATT8FqMNDdotCs94&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=y6lJHsvEGIkn7javATT8FqMNDdotCs94&q=${category}&limit=10`;
     const res = await fetch(url);
     const { data } = await res.json();
 
