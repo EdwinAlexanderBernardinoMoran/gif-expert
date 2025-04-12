21004fe23d7e84d0d6c89c4367cebcb81a43e500
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-12 15:34:36 -0600

    S6- Custom Hook - useFetchGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,26 +1,13 @@
-import { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
 import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
     
-    // const [counter, setcounter] = useState(10)
-    const [images, setImages] = useState([])
-
-    const getImages = async () => {
-        const newImages = await getGifs(category);
-        setImages(newImages);
-    }
-
-    useEffect(() => {
-      getImages();
-    }, [])
+  const { images, isLoading } = useFetchGifs(category);
     
   return (
     <>
       <h3>{category}</h3>
-      {/* <h5>{ counter }</h5>
-      <button onClick={ () => setcounter(counter + 1)}>+1</button> */}
       <div className='card-grid'>
         {
           images.map( (image) => (
